f01e1deb9c27be461f2af70704fd4fa1afc61eee
Markus Feng <contact@markusfeng.com>
2023-02-20 03:01:38 -0500

    Updated tsconfig

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,20 @@
-const rpcIdFindMatch = 'find_match_js';
+function InitModule(
+  ctx: nkruntime.Context,
+  logger: nkruntime.Logger,
+  nk: nkruntime.Nakama,
+  initializer: nkruntime.Initializer
+) {
+  const rpcIdFindMatch = "find_match_js";
+  initializer.registerRpc(rpcIdFindMatch, rpcFindMatch);
+  initializer.registerMatch(moduleName, {
+    matchInit,
+    matchJoinAttempt,
+    matchJoin,
+    matchLeave,
+    matchLoop,
+    matchTerminate,
+    matchSignal,
+  });
 
-let InitModule: nkruntime.InitModule =
-        function(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
-            initializer.registerRpc(rpcIdFindMatch, rpcFindMatch);        
-            initializer.registerMatch(moduleName, {
-                matchInit,
-                matchJoinAttempt,
-                matchJoin,
-                matchLeave,
-                matchLoop,
-                matchTerminate,
-                matchSignal,
-            });
-        
-            logger.info('JavaScript logic loaded.');}
-
+  logger.info("JavaScript logic loaded.");
+}
