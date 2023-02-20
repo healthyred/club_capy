3e3a4480ae4f6f385dbf679656f2f7dab6eebcd1
Markus Feng <contact@markusfeng.com>
2023-02-20 02:29:44 -0500

    Possibly working client

--- ServerConnection.gd
+++ ServerConnection.gd
@@ -6,8 +6,10 @@ var socket : NakamaSocket
 var session : NakamaSession
 var current_match : NakamaRTAPI.Match
 
+var game_client 
+
 func _join_match():
-	var result : NakamaAPI.ApiMatchList = client.list_matches_async(session, 0, 1024, 1, true, "", "")
+	var result : NakamaAPI.ApiMatchList = yield(client.list_matches_async(session, 0, 1024, 1, true, "", ""), "completed")
 	var selected_match = null
 	for m in result.matches:
 		print("%s: %s players", m.match_id, m.size)
@@ -19,16 +21,22 @@ func _join_match():
 	else:
 		print("No matches found, creating a new one")
 		current_match = yield(socket.create_match_async(), "completed")
+	game_client = get_node("GameClient")
+	game_client.initialize(current_match, socket)
 
 func _ready():
 	client.timeout = 10
 	socket = Nakama.create_socket_from(client)
+	print("Socket created.")
 	session = yield(client.authenticate_email_async("me@example.com", "eightchars", "me@example.com"), "completed")
+	print("Authenticated.")
 	var connected : NakamaAsyncResult = yield(socket.connect_async(session), "completed")
+	print("Session connected.")
 	if connected.is_exception():
 		print("An error occurred: %s" % connected)
 		return
 	print("Socket connected.")
+	_join_match()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
