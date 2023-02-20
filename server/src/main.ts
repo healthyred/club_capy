6a00de3f637acd9ca08fb1e8c788603bad27194c
Jian Lu <jhl3@williams.edu>
2023-02-20 02:11:54 -0500

    bad node version on docker but fix soon

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,16 +2,16 @@ const rpcIdFindMatch = 'find_match_js';
 
 let InitModule: nkruntime.InitModule =
         function(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
-            initializer.registerRpc(rpcIdFindMatch, rpcFindMatch);        
-            initializer.registerMatch(moduleName, {
-                matchInit,
-                matchJoinAttempt,
-                matchJoin,
-                matchLeave,
-                matchLoop,
-                matchTerminate,
-                matchSignal,
-            });
+            // initializer.registerRpc(rpcIdFindMatch, rpcFindMatch);        
+            // initializer.registerMatch(moduleName, {
+            //     matchInit,
+            //     matchJoinAttempt,
+            //     matchJoin,
+            //     matchLeave,
+            //     matchLoop,
+            //     matchTerminate,
+            //     matchSignal,
+            // });
         
             logger.info('JavaScript logic loaded.');}
 
